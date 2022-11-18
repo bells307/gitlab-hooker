a28d22f744418417f88b9244fa2ff01c8aeab4cd
bells307 <45540153+bells307@users.noreply.github.com>
2022-11-18 16:21:42 +0300

    Update hook.go

--- internal/service/hook.go
+++ internal/service/hook.go
@@ -19,9 +19,9 @@ func (s *hookService) ProcessMergeRequestHook(mr model.MergeRequestHook) error {
 	var msg string
 
 	if mr.State == "opened" && mr.Action == "open" {
-		msg = fmt.Sprintf("<b>%s</b> создал <b>Merge Request</b> \"%s\" на проекте <i>%s</i>:\n%s", mr.Username, mr.Title, mr.Project, mr.Url)
+		msg = fmt.Sprintf("🔍 <b>%s</b> создал <b>Merge Request</b> \"%s\" на проекте <i>%s</i>:\n%s", mr.Username, mr.Title, mr.Project, mr.Url)
 	} else if mr.State == "merged" && mr.Action == "merge" {
-		msg = fmt.Sprintf("<b>%s</b> слил изменения по <b>Merge Request</b> \"%s\" на проекте <i>%s</i>:\n%s", mr.Username, mr.Title, mr.Project, mr.Url)
+		msg = fmt.Sprintf("✅ <b>%s</b> слил изменения по <b>Merge Request</b> \"%s\" на проекте <i>%s</i>:\n%s", mr.Username, mr.Title, mr.Project, mr.Url)
 	}
 
 	if msg == "" {
